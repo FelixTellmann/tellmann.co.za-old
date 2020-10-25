5a237635bbfd8aa85921f31c43ea08dd4cc0436d
FelixTellmann <felix@tellmann.co.za>
2020-10-25 17:28:40 +0200

    Revert "Footer - Logo Banner done!"
    This reverts commit 277aec30

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,148 +1,63 @@
-import { Footer } from "layouts";
-import { Header } from "layouts/Header";
-import { AppProps } from "next/app";
-import React, { FC, useState } from "react";
-import "styles/animations.scss";
-import "styles/button.scss";
+import { AppProps } from 'next/app';
+import { createContext, FC } from 'react';
+import GoogleFonts from 'next-google-fonts';
+import { BreakpointProvider, useStyledSystem } from "use-styled-system";
+import { DefaultSeo } from 'next-seo';
+import 'reset-css/sass/_reset.scss';
+import { useRouter } from 'next/router';
+import useColorTheme from 'use-color-theme';
 
-import "styles/reset.scss";
-import "styles/responsive.scss";
-import "styles/theme.scss";
-import "styles/typography.scss";
+export const ThemeContext = createContext({ theme: '' });
 
-export type Props = AppProps
-
-export const Root: FC<Props> = ({ pageProps, Component }) => {
-  const [showHeader, setShowHeader] = useState(false);
-  
+export const Root: FC<AppProps> = ({ pageProps, Component }) => {
+  const colorTheme = useColorTheme('light-theme', { classNames: ['light-theme', 'dark-theme', 'blue-theme'] });
+  const { styleJsx } = useStyledSystem(pageProps, {})
+  const router = useRouter();
   return (
+    <>
+      <style jsx>{`
+          ${styleJsx}
+      `}</style>
       <>
-        <style jsx>{`
-          @import 'styles/mixins';
-
-          .main {
-            min-height: calc(100vh);
-            margin: 0 0 0;
-            overflow: hidden;
-            @include responsive('tablet-and-up') {
-              min-height: calc(100vh - var(--header-height));
-              margin: var(--header-height) 0 0;
-            }
-            @include responsive('desktop') {
-              min-height: calc(100vh - var(--header-height));
-              margin: var(--header-height) var(--frame-border) 0;
-            }
-          }
-        
-        `}</style>
-        <Header
-            logo={{ href: "#home", src: "", alt: "Tellmann Logo" }}
-            nav={[
-              { href: "#home", title: "Home", alt: "Hi.", style: "hideOnDesktop" },
-              { href: "#about", title: "About", alt: "Who we are" },
-              { href: "#work", title: "Work", alt: `What we've done` },
-              { href: "#services", title: "Services", alt: `What we can do` },
-              { href: "#contact", title: "Contact", alt: `Get in touch`, style: "button" }
-            ]}
-            address={{ city: "Cape Town", street: "Woodstock Exchange", location: "Office 12", tel: "+27760313590" }}
-            contactNav={[
-              { href: "mailto:info@tellmann.co.za", title: "info@tellmann.co.za", icon: "IoIosMail", nav: true, alt: "Email Us" },
-              { href: "tel: +27760313590", title: "076 031 3590", icon: "IoIosCall", alt: "Call Us", nav: true, style: "iconOnMobile" },
-              { href: "#facebook", icon: "IoLogoFacebook", alt: "Facebook" },
-              { href: "#whatsapp", icon: "IoLogoWhatsapp", alt: "Whatsapp" }
-            ]}
-        >
-        </Header>
-        <main className="main">
-          <Component {...pageProps} />
-          <Footer
-              logo={{ href: "#home", src: "", alt: "Tellmann Logo" }}
-              partners={[
-                { src: "partners-shopify.svg", alt: "Shopify" },
-                { src: "partners-shopify.svg", alt: "Shopify" },
-                { src: "partners-shopify.svg", alt: "Shopify" },
-                { src: "partners-shopify.svg", alt: "Shopify" },
-                { src: "partners-shopify.svg", alt: "Shopify" }
-              ]}
-              nav={[
+        <DefaultSeo
+            title="Tellmann - E-commerce Web development Studio"
+            description="Creator of things that live on the internet - Web developer, writer and entrepreneur."
+            openGraph={{
+              type: 'website',
+              locale: 'en_IE',
+              url: 'https://www.felixtellmann.com/',
+              site_name: 'Felix Tellmann',
+              title: 'Felix Tellmann - Front-end Engineer',
+              description: 'Creator of things that live on the internet - Web developer, writer and entrepreneur.',
+              images: [
                 {
-                  title: "Selling Online made Easy",
-                  responsive: ["col", "col", "hidden"],
-                  items: [
-                    { title: "Who we are", href: "#about" },
-                    { title: "Who we are", href: "#about" },
-                    { title: "Sell online", href: "#about" },
-                    { title: "Shopify Features", href: "#about" },
-                    { title: "Ecommerce Websites", href: "#about" },
-                    { title: "Ecommerce Lessons", href: "#about" }
-                  ]
-                },
-                {
-                  title: "\u00A0",
-                  responsive: ["col", "col", "hidden"],
-                  items: [
-                    { title: "Who we are", href: "#about" },
-                    { title: "Sell online", href: "#about" },
-                    { title: "Shopify Features", href: "#about" },
-                    { title: "Ecommerce Websites", href: "#about" },
-                    { title: "Ecommerce Lessons", href: "#about" },
-                    { title: "Who we are", href: "#about" },
-                    { title: "Sell online", href: "#about" }
-                  ]
-                },
-                {
-                  title: "Tools",
-                  responsive: ["col", "col", "hidden"],
-                  items: [
-                    { title: "Who we are        ", href: "#about" },
-                    { title: "Sell online       ", href: "#about" },
-                    { title: "Shopify Features  ", href: "#about" },
-                    { title: "Ecommerce Websites", href: "#about" },
-                    { title: "Ecommerce Lessons ", href: "#about" },
-                    { title: "Who we are        ", href: "#about" }
-                  ]
-                },
-                {
-                  title: "Support",
-                  responsive: ["col", "col", "hidden"],
-                  items: [
-                    { title: "Who we are        ", href: "#about" },
-                    { title: "Sell online       ", href: "#about" },
-                    { title: "Shopify Features  ", href: "#about" },
-                    { title: "Ecommerce Websites", href: "#about" },
-                    { title: "Ecommerce Lessons ", href: "#about" }
-                  ]
-                },
-                {
-                  title: "Tellmann",
-                  responsive: ["col", "col", "hidden"],
-                  items: [
-                    { title: "Who we are        ", href: "#about" },
-                    { title: "Sell online       ", href: "#about" },
-                    { title: "Shopify Features  ", href: "#about" },
-                    { title: "Ecommerce Websites", href: "#about" },
-                    { title: "Ecommerce Lessons ", href: "#about" }
-                  ]
+                  url: 'https://www.felixtellmann.com/images/og-default.jpg',
+                  alt: 'Felix Tellmann - Front-end Engineer',
+                  width: 1200,
+                  height: 630
                 }
-              ]}
-              contactNav={[
-                { href: "mailto:info@tellmann.co.za", title: "info@tellmann.co.za", icon: "IoIosMail", nav: true, alt: "Email Us" },
-                { href: "tel: +27760313590", title: "076 031 3590", icon: "IoIosCall", alt: "Call Us", nav: true, style: "iconOnMobile" },
-                { href: "#facebook", icon: "IoLogoFacebook", alt: "Facebook" },
-                { href: "#whatsapp", icon: "IoLogoWhatsapp", alt: "Whatsapp" }
-              ]}
-              copyright={`Copyright © 2017 - ${new Date(Date.now()).getFullYear()}.<br>All rights reserved. Tellmann.`}
-          />
-        </main>
-      
+              ]
+            }}
+            twitter={{
+              handle: '@FelixTellmann',
+              site: '@FelixTellmann',
+              cardType: 'summary_large_image'
+            }}
+            canonical={`https://www.felixtellmann.com/${router.pathname}`}
+        />
+        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@400;600;700&display=swap" />
       </>
+      
+     {/* <BreakpointProvider breakPoints={[0, 600, 900, 1200]}>*/}
+        <ThemeContext.Provider value={{ theme: colorTheme.value }}>
+          <div className="page">
+            <Component {...pageProps} />
+          </div>
+        </ThemeContext.Provider>
+      {/*</BreakpointProvider>
+      <Component {...pageProps} />*/}
+    </>
   );
 };
-/*
-Root.getInitialProps = async (appContext) => {
-    // calls page's `getInitialProps` and fills `appProps.pageProps`
-    const appProps = await App.getInitialProps(appContext);
-    return { ...appProps };
-};*/
 
 export default Root;
